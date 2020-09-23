9bd06b9c663b4aeddcc0b356d12d11880bc107a1
Dovile Mart <dovile.martinonyte@gmail.com>
2020-09-23 21:02:21 +0300

    Ensimmäinen Laskin C#:lla

---
+++ DotNet/Laskin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa laskemaan!");
+         /* Console.WriteLine("Anna ensimmäinen luku");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Summa on: " + c);
+            */
+            
+            Console.WriteLine("Anna ensimmäinen luku: ");
+            string luku1 = Console.ReadLine();
+            Console.WriteLine("Anna toinen luku: ");
+            string luku2 = Console.ReadLine();
+            int summa = int.Parse(luku1) + int.Parse(luku2);
+            Console.WriteLine("Summa on: " + summa);
+        }
+    }
+}
