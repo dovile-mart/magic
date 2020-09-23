e0e8a0ee684e125f40c09bb7458c85555b079702
Dovile Mart <dovile.martinonyte@gmail.com>
2020-09-23 20:57:07 +0300

    Ensimmäinen peli C#:lla

---
+++ DotNet/Peli/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Peli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arvaa luku!");
+
+            //arvotaan satunnainen luku, josta tulee pelin oikea vastaus
+            Random satunnainen = new Random();
+            int oikeaLuku = satunnainen.Next(1, 21);
+            //Console.WriteLine(oikeaLuku); //tämä jätetty kommenttina jotta voisi testata koodin toiminnan 
+
+            //annetaan pelaajalle kolme arvauskertaa
+            int arvauskertoja = 0;
+            while (arvauskertoja < 3)
+            {
+                //arvauskerrat kasvaa
+                arvauskertoja++;
+
+                //kysytään pelaajalta arvaus ja muunnetaan se numeroksi
+                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                int arvaus = int.Parse(Console.ReadLine());
+
+                //tarkistetaan, onko annettu luku oikein vai väärin, jos väärin - tulostetaan vihje
+                if (arvaus < oikeaLuku)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }
+                else if (arvaus > oikeaLuku)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                //jos arvattu oikein, tulostetaan onnituleviestin
+                else
+                {
+                    Console.WriteLine("Voitit!");
+                    //silmukka päättyy
+                    break;
+                }
+
+            }
+            //Kun pelaaja on yrittänyt 3 kertaa tai arvannut oikein - tulostetaan ilmoitus pelin loppumisesta 
+            Console.WriteLine("Peli loppui!");
+        }
+    }
+}
